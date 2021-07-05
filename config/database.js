b373f6786b9f772d0b88ae59000a9c35f0593114
Carlos Cobian <rcobianm1600@alumno.ipn.x>
2021-07-04 19:06:36 -0500

    Se agrego sequelize-cli al proyecto

---
+++ config/database.js
@@ -0,0 +1,10 @@
+module.exports = {
+    username: "root",
+    password: "TROLL_face2",
+    database: "plataforma_ipn",
+    host: "localhost",
+    dialect: "mysql",
+    define: {
+        timestamp: false
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const sequelize = require('../database/db');
 
 // Settings
 app.set('port', 8080);
@@ -9,7 +8,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // middlewares
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // routes
 app.use(require('./routes/index'));
@@ -18,12 +18,4 @@ app.use(express.static(__dirname + '/public/'));
 // Listening server
 app.listen(app.get('port'), () => {
     console.log('Server listo', app.get('port'));
-
-    // Conexión a base de datos
-    sequelize.sync( force = false ).then(() => {
-        console.log("Conection Success")
-    }).catch(error =>{
-        console.log("Error en la base de datos", error);
-    })
-
 });
