878a4dbae6bf96c04867a13c3cb2216d60e8c74e
Carlos Cobian <rcobianm1600@alumno.ipn.x>
2021-07-12 22:24:57 -0500

    Se agregó Passport

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.set('view engine', 'pug');
 
 // middlewares
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 // routes
 app.use(require('./routes/routes'));
